a0b0f9a4fb0e9f4a2181a9957d5e5e358d9b330c
kares <self@kares.org>
2011-05-19 13:03:22 +0200

    make sure inherited class variables are excluded

--- lib/less_compiler.rb
+++ lib/less_compiler.rb
@@ -19,36 +19,40 @@ class LessCompiler
   @@check_imports = true
   cattr_accessor :check_imports
 
-  if defined? RAILS_DEFAULT_LOGGER
-    @@logger = RAILS_DEFAULT_LOGGER
+  if defined? Rails.logger
+    @@logger = Rails.logger
   else
     require 'logger' unless defined? Logger
     @@logger = Logger.new($stdout)
   end
   cattr_accessor :logger
 
-  def self.compile_stylesheets(options = {})
-    self.new(options).compile_stylesheets
+  def self.attr_names
+    self.class_variables - Object.class_variables
   end
-
-  def initialize(attrs = {})
-    attrs = attrs.dup
-    self.class.class_variables.each do |cvar|
-      var = cvar[2..-1].to_sym # '@@name' -> :name
-      val = attrs.has_key?(var) ? attrs.delete(var) : self.class.send(var)
-      self.instance_variable_set(:"@#{var}", val)
-    end
-    raise "unsupported option(s): #{attrs.keys.inspect}" unless attrs.empty?
-  end
-
-  attr_reader *self.class_variables.map { |cvar| cvar[2..-1].to_sym }
-
+  
+  attr_reader *self.attr_names.map { |cvar| cvar[2..-1].to_sym }
+  
   def destination_path
     @destination_path || source_path # make sure it defaults to source
   end
 
   def compress?
     !!compress
+  end
+  
+  def self.compile_stylesheets(options = {})
+    self.new(options).compile_stylesheets
+  end
+  
+  def initialize(attrs = {})
+    attrs = attrs.dup
+    self.class.attr_names.each do |cvar|
+      var = cvar[2..-1].to_sym # '@@name' -> :name
+      val = attrs.has_key?(var) ? attrs.delete(var) : self.class.send(var)
+      self.instance_variable_set(:"@#{var}", val)
+    end
+    raise "unsupported option(s): #{attrs.keys.inspect}" unless attrs.empty?
   end
 
   # Updates all stylesheets in the template_location and
@@ -96,57 +100,57 @@ class LessCompiler
   end
 
   private
+  
+    # Check if the specified stylesheet is in need of an update.
+    def needs_update?(less_file)
+      css_file = File.join(destination_path, "#{relative_base(less_file)}.css")
+      return true unless File.exist?(css_file)
+      File.mtime(less_file) > File.mtime(css_file) ||
+      (check_imports && contains_updated_import?(less_file, css_file))
+    end
 
-  # Check if the specified stylesheet is in need of an update.
-  def needs_update?(less_file)
-    css_file = File.join(destination_path, "#{relative_base(less_file)}.css")
-    return true unless File.exist?(css_file)
-    File.mtime(less_file) > File.mtime(css_file) ||
-    (check_imports && contains_updated_import?(less_file, css_file))
-  end
+    # TODO @import file + mtime caching to speed up things ...
 
-  # TODO @import file + mtime caching to speed up things ...
-
-  def contains_updated_import?(less_file, css_file = nil)
-    File.open(less_file) do |file|
-      file.each_line do |line|
-        if line =~ /@import\s*['"](.*)['"]\s*;/
-          import_file = $~[1]
-          #case File.extname(import_file)
-          #  when '.css' then next
-          #  when '' then import_file = "#{import_file}.less"
-          #end
-          if File.extname(import_file).empty?
-            import_file = "#{import_file}.less"
-          end
-          if import_file[0, 1] != '/' # add file's relative path :
-            import_file = File.join(File.dirname(file.path), import_file)
-          end
-          if css_file
-            if File.mtime(import_file) > File.mtime(css_file)
+    def contains_updated_import?(less_file, css_file = nil)
+      File.open(less_file) do |file|
+        file.each_line do |line|
+          if line =~ /@import\s*['"](.*)['"]\s*;/
+            import_file = $~[1]
+            #case File.extname(import_file)
+            #  when '.css' then next
+            #  when '' then import_file = "#{import_file}.less"
+            #end
+            if File.extname(import_file).empty?
+              import_file = "#{import_file}.less"
+            end
+            if import_file[0, 1] != '/' # add file's relative path :
+              import_file = File.join(File.dirname(file.path), import_file)
+            end
+            if css_file
+              if File.mtime(import_file) > File.mtime(css_file)
+                #puts "LESS contains updated import: #{$~[1]}"
+                return true
+              end
+            else
+              if File.mtime(import_file) > File.mtime(less_file)
+                #puts "LESS contains updated import: #{$~[1]}"
+                return true
+              end
+            end
+            if needs_update?(import_file)
               #puts "LESS contains updated import: #{$~[1]}"
               return true
             end
-          else
-            if File.mtime(import_file) > File.mtime(less_file)
-              #puts "LESS contains updated import: #{$~[1]}"
-              return true
-            end
-          end
-          if needs_update?(import_file)
-            #puts "LESS contains updated import: #{$~[1]}"
-            return true
           end
         end
       end
+      false
     end
-    false
-  end
 
-  # Returns the relative base for the given stylesheet
-  def relative_base(stylesheet)
-    path = stylesheet.sub(source_path, '').sub('.less', '')
-    path[0, 1] == '/' ? path[1..-1] : path
-  end
+    # Returns the relative base for the given stylesheet
+    def relative_base(stylesheet)
+      path = stylesheet.sub(source_path, '').sub('.less', '')
+      path[0, 1] == '/' ? path[1..-1] : path
+    end
 
 end
